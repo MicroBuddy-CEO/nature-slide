465648522b6f42db973e2985e44bd600f64a1d68
JewelHFahim <jewelhfahim@gmail.com>
2023-09-08 12:59:19 +0600

    pagination done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="absolute z-[999]">
         <img src={img} alt="" className="h-screen w-full" />
       </div>
-      <div className="absolute z-[999] top-[36%] left-[9.2%]">
+      <div className="absolute z-[999] top-[36%] left-[10%]">
         <h1 className="primaryTitle text-[220px] font-Poppins leading-[200px]">NAT</h1>
         <p className="w-[400px] text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit</p>
         <button className="border px-5 my-6 border-[#89eb8894] rounded-[20px] text-white py-1">Learn More</button>
--- src/components/Slider/SliderContent.jsx
+++ src/components/Slider/SliderContent.jsx
@@ -1,17 +1,13 @@
 import img1 from "../../assets/img1.png";
 // import img2 from "../../assets/img2.png";
 // import img3 from "../../assets/img3.png";
-import img4 from "../../assets/NAT.png";
+// import img4 from "../../assets/NAT.png";
 
 const SliderContent = () => {
   return (
     <div className="relative">
-
-      {/* <div className="absolute text-white text-5xl">
-        My Name is Khan
-      </div> */}
-
       <img src={img1} alt="" className="h-screen w-screen" />
+      <h1 className="text-[250px] font-[900] text-white absolute top-[28%] left-[35%]">USE</h1>
     </div>
   );
 };
